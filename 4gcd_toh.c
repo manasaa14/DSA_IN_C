07e89dfd0dc04b54414208426d82bd7a920a4f4f
manasaa14 <mpmanasa14@gmail.com>
2025-10-23 14:29:05 +0530

    Refactor GCD and Tower of Hanoi functions

--- 4gcd_toh.c
+++ 4gcd_toh.c
@@ -1,41 +1,42 @@
 //GCD OF 2 NOS
 #include <stdio.h>
 #include <stdlib.h>
-int gcd (int a,int b)
-{
-  if (a==b)
-    return a;
-    else if(a>b)
-    return gcd (a-b,b);
-    return gcd (b,a);
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
-int main(){
-int a,b;
-printf("\n read values for a and b :");
-scanf("%d %d", &a,&b);
-printf("%d",gcd(a,b));
-return 0;
+int main() {
+    int a, b;
+    printf("Enter values for a and b: ");
+    scanf("%d%d", &a, &b);
+    //if (a < 0) a = -a;
+    //if (b < 0) b = -b;
+    printf("GCD of both numbers is: %d\n", gcd(a, b));
+    return 0;
 }
+
 
 // TOWER OF HANOI 
 #include <stdio.h>
 #include <stdlib.h>
-void TOH(int n,char s,char d,char t)
-{
-    if(n==1)
-        printf("\nMove %d disc from %c to %c", n,s,d);
-    else
-    {
-        TOH(n-1,s,t,d);
-        printf("\nMove %d disc from %c to %c",n,s,d);
-        TOH(n-1,t,d,s);
+
+void TOH(int n, char s, char d, char t) {
+    if (n == 1)
+        printf("Move disc 1 from %c to %c\n", s, d);
+    else {
+        TOH(n - 1, s, t, d);
+        printf("Move disc %d from %c to %c\n", n, s, d);
+        TOH(n - 1, t, d, s);
     }
 }
-int main()
-{
+
+int main() {
     int n;
-    printf("\nRead number of disc:");
+    printf("Enter number of discs: ");
     scanf("%d", &n);
-    TOH(n,'S','D','T');
+    printf("Steps to solve Tower of Hanoi with %d discs:\n", n);
+    TOH(n, 'S', 'D', 'T');
     return 0;
 }
+
